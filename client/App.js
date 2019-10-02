10b8b0e217e9c674c9a8f15b471b0124ae7e4497
nemanjam <nemanja.mitic.elfak@hotmail.com>
2019-10-02 18:17:15 +0200

    unable to delete photo properly and cant diff camera or gallery

--- client/App.js
+++ client/App.js
@@ -1,13 +1,25 @@
 import React, {useState} from 'react';
+import {StyleSheet, ScrollView, Image, Platform, Clipboard} from 'react-native';
+import RNFS from 'react-native-fs';
+
 import {
-  StyleSheet,
-  View,
-  Text,
-  Image,
+  Container,
+  Header,
+  Title,
+  Content,
+  Footer,
+  FooterTab,
+  Left,
+  Right,
   Button,
-  Platform,
-  Clipboard,
-} from 'react-native';
+  Text,
+  Body,
+  Icon,
+  Toast,
+  Root,
+  Spinner,
+} from 'native-base';
+
 import ImagePicker from 'react-native-image-picker';
 
 const App = () => {
@@ -60,7 +72,8 @@ const App = () => {
 
   function handleUplaodPhoto() {
     uploadFileWithProgress(
-      'http://10.0.2.2:5000/api/upload',
+      // 'http://10.0.2.2:5000/api/upload',
+      'https://sr7.herokuapp.com/api/upload',
       {method: 'POST', body: createFormData(photo, {userId: '123'})},
       event => {
         const _progress = Math.floor(event.loaded / event.total) * 100;
@@ -69,21 +82,56 @@ const App = () => {
     )
       .then(_response => {
         const response = JSON.parse(_response);
-        setUrl(response.url);
-        Clipboard.setString(response.url);
+        const _url = response.url;
+        setUrl(_url);
+        Clipboard.setString(_url);
+        /*
+        RNFS.unlink(photo.path)
+          .then(() => {
+            console.log('FILE DELETED');
+          })
+          .catch(err => {
+            console.log(err.message);
+          });
+*/
         setPhoto(null);
         setProgress(0);
-        alert('Url is in the clipboard.');
+        // alert('Url is in the clipboard.');
+        Toast.show({
+          text: `Url is in the clipboard. Url: ${_url}`,
+          textStyle: {fontSize: 13},
+          buttonText: 'Ok',
+          type: 'success',
+          duration: 3000,
+        });
       })
       .catch(error => {
-        alert(JSON.stringify(error, null, 2));
+        setProgress(0);
+        console.log(error);
+        //alert(error);
+        Toast.show({
+          text: `Error! ${error}`,
+          textStyle: {fontSize: 13},
+          buttonText: 'Okay',
+          type: 'danger',
+          duration: 5000,
+        });
       });
   }
-
+  // adb shell input keyevent 82
   function handleChoosePhoto() {
     setUrl(null);
-    const options = {noData: true, maxWidth: 2000};
+    const options = {
+      noData: true,
+      maxWidth: 2000,
+      mediaType: 'photo',
+      storageOptions: {waitUntilSaved: true},
+    };
     ImagePicker.showImagePicker(options, response => {
+      console.log(response);
+      if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      }
       if (response.uri) {
         setPhoto(response);
       }
@@ -92,32 +140,58 @@ const App = () => {
 
   return (
     <>
-      <View style={styles.view}>
-        {photo && (
-          <>
-            <Image source={{uri: photo.uri}} style={styles.image} />
-            <Text>Progress: {progress}%</Text>
-            <Button title="Upload photo" onPress={handleUplaodPhoto}></Button>
-          </>
-        )}
-        {url && (
-          <>
-            <Text>Url: {url}</Text>
-          </>
-        )}
-        <Button
-          title={!url ? 'Choose Photo' : 'Choose Another Photo'}
-          onPress={handleChoosePhoto}></Button>
-      </View>
+      <Root>
+        <ScrollView>
+          <Container>
+            <Header>
+              <Body>
+                <Title>Upload Image</Title>
+              </Body>
+              <Right />
+            </Header>
+            <Content contentContainerStyle={styles.container}>
+              {progress !== 0 && (
+                <Spinner style={styles.spinner} color="blue" />
+              )}
+              {photo && (
+                <>
+                  <Image source={{uri: photo.uri}} style={styles.image} />
+                  <Text>Progress: {progress}%</Text>
+                  <Button
+                    primary
+                    onPress={handleUplaodPhoto}
+                    disabled={progress !== 0}>
+                    <Icon name="cloud-upload" />
+                    <Text>Upload photo</Text>
+                  </Button>
+                </>
+              )}
+              <Button
+                primary
+                onPress={handleChoosePhoto}
+                disabled={progress !== 0}>
+                <Icon name="camera" />
+                <Text>{!photo ? 'Choose Photo' : 'Choose Another Photo'}</Text>
+              </Button>
+            </Content>
+          </Container>
+        </ScrollView>
+      </Root>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  view: {
+  container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
+  },
+  spinner: {
+    position: 'absolute',
+    zIndex: 1000,
+    top: '25%',
+    left: '45%',
   },
   image: {
     width: 300,
